d93c4fbcecedac0f2a122d612302a5cfd5446796
kwabsntim <daffourkwabena@gmail.com>
2025-07-05 09:06:58 +0000

    addition of Executive route

--- eleesa.py
+++ eleesa.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('eleesa.html')
+@app.route("/executives")
+def executives():
+    return render_template('executives.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
